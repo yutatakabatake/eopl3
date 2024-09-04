beae01f814fb51628d23732693551b4445b60b9d
yutatakabatake <yutatakabatake@gmail.com>
2024-09-04 20:12:53 +0900

    ex12完成

--- exercise2-12.rkt
+++ exercise2-12.rkt
@@ -5,52 +5,39 @@
 ;empty-stack : () -> Stack
 (define empty-stack
   (lambda ()
-    (lambda (top-var)
-      (report-no-binding-found top-var))))
+    (lambda (cmd)
+      (report-no-binding-found cmd))))
 
+;push : Val × Stack -> Stack
+(define push
+  (lambda (val stk)
+    (lambda (cmd)
+      (cond
+        ((equal? cmd top)
+         val)
+        ((equal? cmd pop)
+         stk)))))
+
+;pop : Stack -> Stack
+(define pop
+  (lambda (stk)
+    (stk pop)))
+
+;top : Stack -> SchemeVal
+(define top
+  (lambda (stk)
+    (stk top)))
 
 (define report-no-binding-found
-  (lambda (search-var)
-    (eopl:error 'apply-env "No binding for -s" search-var)))
+  (lambda (cmd)
+    (eopl:error 'empty-stack "No binding for -s" cmd)))
 
 (define report-invalid-env
-  (lambda (env)
-    (eopl:error 'apply-env "Bad environment: -s" env)))
+  (lambda (stk)
+    (eopl:error 'apply-env "Bad environment: -s" stk)))
 
+(define report-stack-is-empty
+  (lambda (ops)
+    (eopl:error 'ops "Stack is empty")))
 
-; (define report-stack-is-empty
-;   (lambda (ops)
-;     (eopl:error ops "Stack is empty")))
-
-; (define empty-stack
-;   (lambda ()
-;     (lambda (ops)
-;       (if (equal? ops 'empty?) #t
-;           (report-stack-is-empty ops)))))
-
-; (define push
-;   (lambda (val stack)
-;     (lambda (ops)
-;       (cond [(equal? ops 'top) val]
-;             [(equal? ops 'empty?) #f]
-;             [(equal? ops 'pop) stack]))))
-
-; (define top
-;   (lambda (stack)
-;     (stack 'top)))
-
-; (define pop
-;   (lambda (stack)
-;     (stack 'pop)))
-
-; (define empty-stack?
-;   (lambda (stack)
-;     (stack 'empty?)))
-
-; (display
-;  (and (equal? (empty-stack? (empty-stack)) #t)
-;      (equal? (empty-stack? (push 1 (empty-stack))) #f)
-;      (equal? (top (push 1 (empty-stack))) 1)
-;      (equal? (empty-stack? (pop (push 1 (empty-stack)))) #t)
-;      (equal? (top (push 1 (push 2 (push 3 (empty-stack))))) 1)
-;      (equal? (top (pop (push 1 (push 2 (push 3 (empty-stack)))))) 2)))
+;オブザーバを基準に考えて，引数を一つ受け取って値を返す手続きとして環境のインタフェースを作る
