be3770c0cf7872ff41386b55733ec592a5bc738c
yutatakabatake <yutatakabatake@gmail.com>
2024-10-03 17:32:18 +0900

    swap-test追加

--- 4-3-IMPLICIT-REFS.rkt
+++ 4-3-IMPLICIT-REFS.rkt
@@ -434,3 +434,17 @@
         set x = 100;
         x
         end")
+
+(define swap-test
+  "let swap = proc (x) proc (y) 
+                let temp = x
+                  in begin
+                      set x = y;
+                      set y = temp
+                     end
+      in let a = 33
+         in let b = 44
+            in begin
+                ((swap a) b);
+                -(a,b) 
+               end")
