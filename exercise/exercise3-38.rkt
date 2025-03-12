3dd1d210ac450f4e48c21f6155ca1e78acf9bb8d
yutatakabatake <yutatakabatake@gmail.com>
2025-03-12 16:36:38 +0900

    ex3-38完成

---
+++ exercise/exercise3-38.rkt
@@ -0,0 +1,394 @@
+#lang eopl
+
+; Lexical addressingにcondを拡張
+
+; Senv = Listof(Sym)
+; Lexaddr = N
+
+; empty-senv : () → Senv
+(define empty-senv
+  (lambda ()
+    '()))
+
+; extend-senv : Var × Senv → Senv
+(define extend-senv
+  (lambda (var senv)
+    (cons var senv)))
+
+; apply-senv : Senv × Var → Lexaddr
+(define apply-senv
+  (lambda (senv var)
+    (cond
+      ((null? senv)
+       (report-unbound-var var))
+      ((eqv? var (car senv))
+       0)
+      (else
+       (+ 1 (apply-senv (cdr senv) var))))))
+
+; init-senv : () → Senv
+(define init-senv
+  (lambda ()
+    (extend-senv 'i
+                 (extend-senv 'v
+                              (extend-senv 'x
+                                           (empty-senv))))))
+
+
+; nameless-environment? : SchemeVal → Bool
+(define nameless-environment?
+  (lambda (x)
+    ((list-of expval?) x)))
+
+; empty-nameless-env : () → Nameless-env
+(define empty-nameless-env
+  (lambda ()
+    '()))
+
+; extend-nameless-env : ExpVal × Nameless-env → Nameless-env
+(define extend-nameless-env
+  (lambda (val nameless-env)
+    (cons val nameless-env)))
+
+; apply-nameless-env : Nameless-env × Lexaddr → ExpVal
+(define apply-nameless-env
+  (lambda (nameless-env n)
+    (list-ref nameless-env n)))
+
+
+(define init-nameless-env
+  (lambda ()
+    ; (extend-nameless-env
+    ;  (num-val 1)			; was i
+    ;  (extend-nameless-env
+    ;   (num-val 5)			; was v
+    ;   (extend-nameless-env
+    ;    (num-val 10)			; was x
+    (empty-nameless-env)))
+;    )))
+
+
+;Expressed values
+(define-datatype expval expval?
+  (num-val
+   (num number?))
+  (bool-val
+   (bool boolean?))
+  (proc-val
+   (proc proc?)))
+
+;interface
+;expval->num : ExpVal → Int
+(define expval->num
+  (lambda (val)
+    (cases expval val
+      (num-val (num) num)
+      (else (report-expval-extractor-error 'num val)))))
+
+;expval->bool : ExpVal → Bool
+(define expval->bool
+  (lambda (val)
+    (cases expval val
+      (bool-val (bool) bool)
+      (else (report-expval-extractor-error 'bool val)))))
+
+; expval->proc : ExpVal -> Proc
+(define expval->proc
+  (lambda (v)
+    (cases expval v
+      (proc-val (proc) proc)
+      (else (report-expval-extractor-error 'proc v)))))
+
+; procedure : Nameless-exp × Nameless-env → Proc
+(define-datatype proc proc?
+  (procedure
+   (body expression?)
+   (saved-nameless-env nameless-environment?)))
+
+; apply-procedure : Proc × ExpVal → ExpVal
+(define apply-procedure
+  (lambda (proc1 val)
+    (cases proc proc1
+      (procedure (body saved-nameless-env)
+                 (value-of body
+                           (extend-nameless-env val saved-nameless-env))))))
+
+
+;Syntax data types
+;BNFでの文法
+(define-datatype program program?
+  (a-program
+   (exp1 expression?)))
+
+(define-datatype expression expression?
+  (const-exp
+   (num number?))
+  (diff-exp
+   (exp1 expression?)
+   (exp2 expression?))
+  (zero?-exp
+   (exp1 expression?))
+  (if-exp
+   (exp1 expression?)
+   (exp2 expression?)
+   (exp3 expression?))
+  (var-exp
+   (var identifier?))
+  (let-exp
+   (var identifier?)
+   (exp1 expression?)
+   (body expression?))
+  (proc-exp
+   (var identifier?)
+   (body expression?))
+  (call-exp
+   (rator expression?)
+   (rand expression?))
+  (nameless-var-exp
+   (num number?))
+  (nameless-let-exp
+   (exp1 expression?)
+   (body expression?))
+  (nameless-proc-exp
+   (body expression?))
+
+  (cond-exp
+   (conditions (list-of expression?))
+   (bodies (list-of expression?))))
+
+; translation-of-program : Program → Nameless-program
+(define translation-of-program
+  (lambda (pgm)
+    (cases program pgm
+      (a-program (exp1)
+                 (a-program
+                  (translation-of exp1 (init-senv)))))))
+
+; translation-of : Exp × Senv → Nameless-exp
+(define translation-of
+  (lambda (exp senv)
+    (cases expression exp
+      (const-exp (num) (const-exp num))
+      (diff-exp (exp1 exp2)
+                (diff-exp
+                 (translation-of exp1 senv)
+                 (translation-of exp2 senv)))
+      (zero?-exp (exp1)
+                 (zero?-exp
+                  (translation-of exp1 senv)))
+      (if-exp (exp1 exp2 exp3)
+              (if-exp
+               (translation-of exp1 senv)
+               (translation-of exp2 senv)
+               (translation-of exp3 senv)))
+      (var-exp (var)
+               (nameless-var-exp
+                (apply-senv senv var)))
+      (let-exp (var exp1 body)
+               (nameless-let-exp
+                (translation-of exp1 senv)
+                (translation-of body
+                                (extend-senv var senv))))
+      (proc-exp (var body)
+                (nameless-proc-exp
+                 (translation-of body
+                                 (extend-senv var senv))))
+      (call-exp (rator rand)
+                (call-exp
+                 (translation-of rator senv)
+                 (translation-of rand senv)))
+
+      (cond-exp (conditions bodies)
+                (let ([nameless-conditions (map (lambda (cond) (translation-of cond senv)) conditions)]
+                      [nameless-bodies (map (lambda (body) (translation-of body senv)) bodies)])
+                  (cond-exp nameless-conditions nameless-bodies)))
+
+      (else
+       (report-invalid-source-expression exp)))))
+
+; value-of-program : Nameless-program → ExpVal
+(define value-of-program
+  (lambda (pgm)
+    (cases program pgm
+      (a-program (exp1)
+                 (value-of exp1 (init-nameless-env))))))
+
+; value-of : Nameless-exp × Nameless-env → ExpVal
+(define value-of
+  (lambda (exp nameless-env)
+    (cases expression exp
+      (const-exp (num) (num-val num))
+      (diff-exp (exp1 exp2)
+                (let ((val1 (value-of exp1 nameless-env))
+                      (val2 (value-of exp2 nameless-env)))
+                  (let ((num1 (expval->num val1))
+                        (num2 (expval->num val2)))
+                    (num-val
+                     (- num1 num2)))))
+      (zero?-exp (exp1)
+                 (let ((val1 (value-of exp1 nameless-env)))
+                   (let ((num1 (expval->num val1)))
+                     (if (zero? num1)
+                         (bool-val #t)
+                         (bool-val #f)))))
+      (if-exp (exp1 exp2 exp3)
+              (let ((val1 (value-of exp1 nameless-env)))
+                (if (expval->bool val1)
+                    (value-of exp2 nameless-env)
+                    (value-of exp3 nameless-env))))
+      (call-exp (rator rand)
+                (let ((proc (expval->proc (value-of rator nameless-env)))
+                      (arg (value-of rand nameless-env)))
+                  (apply-procedure proc arg)))
+      (nameless-var-exp (n)
+                        (apply-nameless-env nameless-env n))
+      (nameless-let-exp (exp1 body)
+                        (let ((val (value-of exp1 nameless-env)))
+                          (value-of body
+                                    (extend-nameless-env val nameless-env))))
+      (nameless-proc-exp (body)
+                         (proc-val
+                          (procedure body nameless-env)))
+
+      (cond-exp (conditions bodies)
+                (if (null? conditions)
+                    (report-invalid-translated-expression exp)
+                    (let ([condition (car conditions)]
+                          [body (car bodies)])
+                      (if (expval->bool (value-of condition nameless-env))
+                          (value-of body nameless-env)
+                          (value-of (cond-exp (cdr conditions) (cdr bodies)) nameless-env)))))
+
+      (else
+       (report-invalid-translated-expression exp)))))
+
+
+
+
+(define scanner-spec-nameless
+  '((whitespace (whitespace) skip) ; Skip the whitespace
+    ; Any arbitrary string of characters following a "%" upto a newline are skipped.
+    (comment ("%" (arbno (not #\newline))) skip)
+    ; An identifier is a letter followed by an arbitrary number of contiguous digits,
+    ; letters, or specified punctuation characters.
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    ; A number is any digit followed by an arbitrary number of digits
+    ; or a "-" followed by a digit and an arbitrary number of digits.
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)))
+
+(define grammar-nameless
+  '((program (expression) a-program)
+
+    (expression (number) const-exp)
+    (expression (identifier) var-exp)
+
+    ; zero?-exp : zero?(a)
+    (expression
+     ("zero?" "(" expression ")")
+     zero?-exp)
+
+    ; diff-exp : -(a,b) -> (- a b)
+    (expression
+     ("-" "(" expression "," expression ")")
+     diff-exp)
+
+    ; if-exp : if a then b else c
+    (expression
+     ("if" expression "then" expression "else" expression)
+     if-exp)
+
+    ; let-exp : let y = a in b
+    (expression
+     ("let" identifier "=" expression "in" expression)
+     let-exp)
+
+    ; proc-exp : proc(a) b
+    (expression
+     ("proc" "(" identifier ")" expression)
+     proc-exp)
+
+    ; call-exp : (a b)
+    (expression
+     ("(" expression expression ")")
+     call-exp)
+
+    ; cond-exp : cond (a ==> b) end
+    (expression
+     ("cond" (arbno expression "==>" expression) "end")
+     cond-exp)
+
+    (expression
+     ("%lexref" number)
+     nameless-var-exp)
+
+    (expression
+     ("%let" expression "in" expression)
+     nameless-let-exp)
+
+    (expression
+     ("%lexproc" expression)
+     nameless-proc-exp)
+    ))
+
+
+; run : String → ExpVal
+(define run
+  (lambda (string)
+    (value-of-program
+     (translation-of-program
+      (scan&parse string)))))
+
+; translate : String → Nameless-program
+(define translate
+  (lambda (string)
+    (translation-of-program
+     (scan&parse string))))
+
+(define scan&parse
+  (sllgen:make-string-parser scanner-spec-nameless grammar-nameless))
+
+(define repl
+  (sllgen:make-rep-loop "--> "
+                        (lambda (pgm) (value-of-program (translation-of-program pgm)))
+                        (sllgen:make-stream-parser scanner-spec-nameless grammar-nameless)))
+
+(define identifier?
+  (lambda (x)
+    (and
+     (symbol? x)
+     (not (eqv? x 'lambda)))))
+
+(define report-unbound-var
+  (lambda (var)
+    (eopl:error 'translation-of "unbound variable in code: ~s" var)))
+
+(define report-invalid-source-expression
+  (lambda (exp)
+    (eopl:error 'value-of
+                "Illegal expression in source code: ~s" exp)))
+
+(define report-expval-extractor-error
+  (lambda (expected val)
+    (eopl:error 'expval-extractor "Expected a ~s, got ~s" expected val)))
+
+(define report-invalid-translated-expression
+  (lambda (exp)
+    (eopl:error 'value-of
+                "Illegal expression in translated code: ~s" exp)))
+
+
+(define test
+  "let x = 37
+            in proc (y)
+                let z = -(y,x)
+                in -(x,y)")
+
+(define cond-test
+  "let f = proc(x)
+            cond zero?(x) ==> 123
+                 zero?(-(x,1)) ==> 456
+            end
+    in (f 1)")
